688186b60ad9265d9f1ea9a4271164dec244c4d4
Ivan Konovalov <zzoomzoone@gmail.com>
2023-02-24 05:33:26 +0400

    step 2: 'Patch gendiff' 	-add Eslint as dev dependence 	-add argument for programm

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+import { Command } from 'commander';
+
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and show a difference.')
-    .version('0.0.1');
+  .name('gendiff')
+  .description('Compares two configuration files and show a difference.')
+  .version('0.0.2')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
